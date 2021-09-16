02a30b74fa2ce64bce255a24ec2e8bf2795a1098
Takao Ikoma <ikoma-takao@city.nara.lg.jp>
2021-09-16 17:18:57 +0900

    added osaka pref data etc.

---
+++ osaka/data/normalizename.sh
@@ -0,0 +1,315 @@
+mv 0101.xlsx 20210101.xlsx
+mv 0102.xlsx 20210102.xlsx
+mv 0103.xlsx 20210103.xlsx
+mv 0104.xlsx 20210104.xlsx
+mv 0105.xlsx 20210105.xlsx
+mv 0106\ .xlsx 20210106.xlsx
+mv 0107.xlsx 20210107.xlsx
+mv 0108.xlsx 20210108.xlsx
+mv 0109.xlsx 20210109.xlsx
+mv 0110.xlsx 20210110.xlsx
+mv 0111.xlsx 20210111.xlsx
+mv 0112\ .xlsx 20210112.xlsx
+mv 0113\ .xlsx 20210113.xlsx
+mv 0114.xlsx 20210114.xlsx
+mv 0115.xlsx 20210115.xlsx
+mv 0116\ .xlsx 20210116.xlsx
+mv 0117.xlsx 20210117.xlsx
+mv 0118.xlsx 20210118.xlsx
+mv 0119.xlsx 20210119.xlsx
+mv 0120.xlsx 20210120.xlsx
+mv 0121.xlsx 20210121.xlsx
+mv 0122_2.xlsx 20210122.xlsx
+mv 0123.xlsx 20210123.xlsx
+mv 0124.xlsx 20210124.xlsx
+mv 0125.xlsx 20210125.xlsx
+mv 0126.xlsx 20210126.xlsx
+mv 0127-2.xlsx 20210127.xlsx
+mv 0128_2\ .xlsx 20210128.xlsx
+mv 0129\ .xlsx 20210129.xlsx
+mv 0130.xlsx 20210130.xlsx
+mv 0131.xlsx 20210131.xlsx
+mv 0201.xlsx 20210201.xlsx
+mv 0202.xlsx 20210202.xlsx
+mv 0203.xlsx 20210203.xlsx
+mv 0204_2.xlsx 20210204.xlsx
+mv 0205.xlsx 20210205.xlsx
+mv 0206.xlsx 20210206.xlsx
+mv 0207.xlsx 20210207.xlsx
+mv 0208.xlsx 20210208.xlsx
+mv 0209\ .xlsx 20210209.xlsx
+mv 0210\ .xlsx 20210210.xlsx
+mv 0211.xlsx 20210211.xlsx
+mv 0212.xlsx 20210212.xlsx
+mv 0213.xlsx 20210213.xlsx
+mv 0214.xlsx 20210214.xlsx
+mv 0215.xlsx 20210215.xlsx
+mv 0216.xlsx 20210216.xlsx
+mv 0217.xlsx 20210217.xlsx
+mv 0218.xlsx 20210218.xlsx
+mv 0219\ .xlsx 20210219.xlsx
+mv 0220.xlsx 20210220.xlsx
+mv 0221.xlsx 20210221.xlsx
+mv 0222.xlsx 20210222.xlsx
+mv 0223.xlsx 20210223.xlsx
+mv 0224.xlsx 20210224.xlsx
+mv 0225.xlsx 20210225.xlsx
+mv 0226.xlsx 20210226.xlsx
+mv 0227.xlsx 20210227.xlsx
+mv 0228.xlsx 20210228.xlsx
+mv 0301.xlsx 20210301.xlsx
+mv 0302.xlsx 20210302.xlsx
+mv 0303.xlsx 20210303.xlsx
+mv 0304.xlsx 20210304.xlsx
+mv 0305.xlsx 20210305.xlsx
+mv 0306.xlsx 20210306.xlsx
+mv syusei0307.xlsx 20200307.xlsx
+mv 0308.xlsx 20210308.xlsx
+mv 0309.xlsx 20210309.xlsx
+mv 0310.xlsx 20210310.xlsx
+mv 0311\ .xlsx 20210311.xlsx
+mv 0312\ .xlsx 20210312.xlsx
+mv 0313\ .xlsx 20210313.xlsx
+mv 0314\ .xlsx 20210314.xlsx
+mv 0315\ .xlsx 20210315.xlsx
+mv 0316\ .xlsx 20210316.xlsx
+mv 0317\ .xlsx 20210317.xlsx
+mv 0318.xlsx 20210318.xlsx
+mv 0319.xlsx 20210319.xlsx
+mv 0320.xlsx 20210320.xlsx
+mv 0321.xlsx 20210321.xlsx
+mv 0322\ .xlsx 20210322.xlsx
+mv 0323.xlsx 20210323.xlsx
+mv 0324.xlsx 20210324.xlsx
+mv 0325.xlsx 20210325.xlsx
+mv 0326.xlsx 20210326.xlsx
+mv 0327.xlsx 20210327.xlsx
+mv 0328.xlsx 20210328.xlsx
+mv 0329.xlsx 20210329.xlsx
+mv 0330.xlsx 20210330.xlsx
+mv 0331.xlsx 20210331.xlsx
+mv 0401.xlsx 20210401.xlsx
+mv 0402.xlsx 20210402.xlsx
+mv 0403.xlsx 20210403.xlsx
+mv 0404.xlsx 20210404.xlsx
+mv 0405.xlsx 20210405.xlsx
+mv 0406.xlsx 20210406.xlsx
+mv 0407.xlsx 20210407.xlsx
+mv 0408.xlsx 20210408.xlsx
+mv 0409.xlsx 20210409.xlsx
+mv 0410.xlsx 20210410.xlsx
+mv 0411.xlsx 20210411.xlsx
+mv 0412.xlsx 20210412.xlsx
+mv 0413.xlsx 20210413.xlsx
+mv 0414.xlsx 20210414.xlsx
+mv 0415.xlsx 20210415.xlsx
+mv 0416.xlsx 20210416.xlsx
+mv 0417.xlsx 20210417.xlsx
+mv 0418.xlsx 20210418.xlsx
+mv 0419.xlsx 20210419.xlsx
+mv 0420.xlsx 20210420.xlsx
+mv 0421.xlsx 20210421.xlsx
+mv 0422.xlsx 20210422.xlsx
+mv 0423.xlsx 20210423.xlsx
+mv 0424.xlsx 20210424.xlsx
+mv 0425.xlsx 20210425.xlsx
+mv 0426.xlsx 20210426.xlsx
+mv 0427.xlsx 20210427.xlsx
+mv 0428.xlsx 20210428.xlsx
+mv 0429.xlsx 20210429.xlsx
+mv 0430.xlsx 20210430.xlsx
+mv 0501.xlsx 20210501.xlsx
+mv 0502.xlsx 20210502.xlsx
+mv 0503.xlsx 20210503.xlsx
+mv 0504.xlsx 20210504.xlsx
+mv 0505.xlsx 20210505.xlsx
+mv 0506.xlsx 20210506.xlsx
+mv 0507.xlsx 20210507.xlsx
+mv 0508.xlsx 20210508.xlsx
+mv 0509.xlsx 20210509.xlsx
+mv 0510.xlsx 20210510.xlsx
+mv 0511.xlsx 20210511.xlsx
+mv 0512.xlsx 20210512.xlsx
+mv 0513.xlsx 20210513.xlsx
+mv 0514.xlsx 20210514.xlsx
+mv 0515.xlsx 20210515.xlsx
+mv 0516.xlsx 20210516.xlsx
+mv 0517\ -\ .xlsx 20210517.xlsx
+mv 0518.xlsx 20210518.xlsx
+mv 0519.xlsx 20210519.xlsx
+mv 0520.xlsx 20210520.xlsx
+mv 0521.xlsx 20210521.xlsx
+mv 0522.xlsx 20210522.xlsx
+mv 0523.xlsx 20210523.xlsx
+mv 0524.xlsx 20210524.xlsx
+mv 0525.xlsx 20210525.xlsx
+mv 0526.xlsx 20210526.xlsx
+mv 0527.xlsx 20210527.xlsx
+mv 0528.xlsx 20210528.xlsx
+mv 0529.xlsx 20210529.xlsx
+mv 0530.xlsx 20210530.xlsx
+mv 0531.xlsx 20210531.xlsx
+mv 0601.xlsx 20210601.xlsx
+mv 0602.xlsx 20210602.xlsx
+mv 0603.xlsx 20210603.xlsx
+mv 0604.xlsx 20210604.xlsx
+mv 0605.xlsx 20210605.xlsx
+mv 0606.xlsx 20210606.xlsx
+mv 0607.xlsx 20210607.xlsx
+mv 0608.xlsx 20210608.xlsx
+mv 0609.xlsx 20210609.xlsx
+mv 0610.xlsx 20210610.xlsx
+mv 0611.xlsx 20210611.xlsx
+mv 0612.xlsx 20210612.xlsx
+mv 0613.xlsx 20210613.xlsx
+mv 0614.xlsx 20210614.xlsx
+mv 0615.xlsx 20210615.xlsx
+mv 0616.xlsx 20210616.xlsx
+mv 0617.xlsx 20210617.xlsx
+mv 0618.xlsx 20210618.xlsx
+mv 0619.xlsx 20210619.xlsx
+mv 0620.xlsx 20210620.xlsx
+mv 0621.xlsx 20210621.xlsx
+mv 0622.xlsx 20210622.xlsx
+mv 0623.xlsx 20210623.xlsx
+mv 0624.xlsx 20210624.xlsx
+mv 0625.xlsx 20210625.xlsx
+mv 0626.xlsx 20210626.xlsx
+mv 0627.xlsx 20210627.xlsx
+mv 0628.xlsx 20210628.xlsx
+mv 0629.xlsx 20210629.xlsx
+mv 0630.xlsx 20210630.xlsx
+mv 0701.xlsx 20210701.xlsx
+mv 0702.xlsx 20210702.xlsx
+mv 0703.xlsx 20210703.xlsx
+mv 0704.xlsx 20210704.xlsx
+mv 0705.xlsx 20210705.xlsx
+mv 0706.xlsx 20210706.xlsx
+mv 0707.xlsx 20210707.xlsx
+mv 0708.xlsx 20210708.xlsx
+mv 0709.xlsx 20210709.xlsx
+mv 0710.xlsx 20210710.xlsx
+mv 0711.xlsx 20210711.xlsx
+mv 0712.xlsx 20210712.xlsx
+mv 0713.xlsx 20210713.xlsx
+mv 0714.xlsx 20210714.xlsx
+mv 0715.xlsx 20210715.xlsx
+mv 0716.xlsx 20210716.xlsx
+mv 0717.xlsx 20210717.xlsx
+mv 0718.xlsx 20210718.xlsx
+mv 0719.xlsx 20210719.xlsx
+mv 0720.xlsx 20210720.xlsx
+mv 0721.xlsx 20210721.xlsx
+mv 0722.xlsx 20210722.xlsx
+mv 0723.xlsx 20210723.xlsx
+mv 0724.xlsx 20210724.xlsx
+mv 0725.xlsx 20210725.xlsx
+mv 0726.xlsx 20210726.xlsx
+mv 0727.xlsx 20210727.xlsx
+mv 0728.xlsx 20210728.xlsx
+mv 0729.xlsx 20210729.xlsx
+mv 0730.xlsx 20210730.xlsx
+mv 0731.xlsx 20210731.xlsx
+mv 0801.xlsx 20210801.xlsx
+mv 0802.xlsx 20210802.xlsx
+mv 0803.xlsx 20210803.xlsx
+mv 0804.xlsx 20210804.xlsx
+mv 0805.xlsx 20210805.xlsx
+mv 0806.xlsx 20210806.xlsx
+mv 0807.xlsx 20210807.xlsx
+mv 0808\ .xlsx 20210808.xlsx
+mv 0809.xlsx 20210809.xlsx
+mv 0810.xlsx 20210810.xlsx
+mv 0811.xlsx 20210811.xlsx
+mv 0812.xlsx 20210812.xlsx
+mv 0813.xlsx 20210813.xlsx
+mv 0814.xlsx 20210814.xlsx
+mv 0815.xlsx 20210815.xlsx
+mv 0816.xlsx 20210816.xlsx
+mv 0817.xlsx 20210817.xlsx
+mv 0819.xlsx 20210819.xlsx
+mv 0820.xlsx 20210820.xlsx
+mv 0821.xlsx 20210821.xlsx
+mv 0822.xlsx 20210822.xlsx
+mv 0823.xlsx 20210823.xlsx
+mv 0824.xlsx 20210824.xlsx
+mv 0825.xlsx 20210825.xlsx
+mv 0826.xlsx 20210826.xlsx
+mv 0827.xlsx 20210827.xlsx
+mv 0828\ .xlsx 20210828.xlsx
+mv 0829\ .xlsx 20210829.xlsx
+mv 0830\ .xlsx 20210830.xlsx
+mv 0831\ .xlsx 20210831.xlsx
+mv 0901-2.xlsx 20210901.xlsx
+mv 0902\ .xlsx 20210902.xlsx
+mv 0903\ .xlsx 20210903.xlsx
+mv 0904\ .xlsx 20210904.xlsx
+mv 0905.xlsx 20210905.xlsx
+mv 0906\ .xlsx 20210906.xlsx
+mv 0907.xlsx 20210907.xlsx
+mv 0908.xlsx 20210908.xlsx
+mv 0909\ .xlsx 20210909.xlsx
+mv 0910.xlsx 20210910.xlsx
+mv 0911.xlsx 20210911.xlsx
+mv 0912.xlsx 20210912.xlsx
+mv 0913.xlsx 20210913.xlsx
+mv 1102syusei\ .xlsx 20211102.xlsx
+mv 1103\ .xlsx 20211103.xlsx
+mv 1104.xlsx 20211104.xlsx
+mv 1105\ .xlsx 20211105.xlsx
+mv 1106shusei.xlsx 20211106.xlsx
+mv 1107.xlsx 20211107.xlsx
+mv 1108.xlsx 20211108.xlsx
+mv 1109.xlsx 20211109.xlsx
+mv 1110.xlsx 20211110.xlsx
+mv 1111.xlsx 20211111.xlsx
+mv 1112\ .xlsx 20211112.xlsx
+mv 1113\ .xlsx 20211113.xlsx
+mv 1114\ .xlsx 20211114.xlsx
+mv 1115shusei.xlsx 20211115.xlsx
+mv 1116.xlsx 20211116.xlsx
+mv 1117.xlsx 20211117.xlsx
+mv 1118syusei.xlsx 20211118.xlsx
+mv 1119.xlsx 20211119.xlsx
+mv 1120.xlsx 20211120.xlsx
+mv 1121.xlsx 20211121.xlsx
+mv 1122syusei.xlsx 20211122.xlsx
+mv 1123syusei.xlsx 20211123.xlsx
+mv 1124\ .xlsx 20211124.xlsx
+mv 1125\ .xlsx 20211125.xlsx
+mv 1126.xlsx 20211126.xlsx
+mv 1127\ .xlsx 20211127.xlsx
+mv 1128.xlsx 20211128.xlsx
+mv 1129syusei.xlsx 20211129.xlsx
+mv 1130\ .xlsx 20211130.xlsx
+mv 1201.xlsx 20201201.xlsx
+mv 1202.xlsx 20201202.xlsx
+mv 1203.xlsx 20201203.xlsx
+mv 1204\ .xlsx 20201204.xlsx
+mv 1205.xlsx 20201205.xlsx
+mv 1206.xlsx 20201206.xlsx
+mv 1207.xlsx 20201207.xlsx
+mv 1208.xlsx 20201208.xlsx
+mv 1209\ .xlsx 20201209.xlsx
+mv 1210.xlsx 20201210.xlsx
+mv 1211.xlsx 20201211.xlsx
+mv 1212\ .xlsx 20201212.xlsx
+mv 1213\ .xlsx 20201213.xlsx
+mv 1214.xlsx 20201214.xlsx
+mv 1215.xlsx 20201215.xlsx
+mv 1216.xlsx 20201216.xlsx
+mv 1217.xlsx 20201217.xlsx
+mv 1218.xlsx 20201218.xlsx
+mv 1219.xlsx 20201219.xlsx
+mv 1220.xlsx 20201220.xlsx
+mv 1221.xlsx 20201221.xlsx
+mv 1222.xlsx 20201222.xlsx
+mv 1223.xlsx 20201223.xlsx
+mv 1224.xlsx 20201224.xlsx
+mv 1225\ .xlsx 20201225.xlsx
+mv 1226.xlsx 20201226.xlsx
+mv 1227.xlsx 20201227.xlsx
+mv 1228.xlsx 20201228.xlsx
+mv 1229\ .xlsx 20201229.xlsx
+mv 1230\ .xlsx 20201230.xlsx
+mv 1231\ .xlsx 20201231.xlsx
